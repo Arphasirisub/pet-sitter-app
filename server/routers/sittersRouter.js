88e776c04a8f44bdde13de79236c8a4d167e0ada
Am <arphasirisubb@gmail.com>
2024-01-31 21:31:46 +0700

    test(sitterRouter): test router

--- server/routers/sittersRouter.js
+++ server/routers/sittersRouter.js
@@ -2,3 +2,13 @@ import { Router } from "express";
 import supabase from "../utills/supabase.js";
 
 export const sittersRouter = Router();
+
+sittersRouter.get("/", async (req, res) => {
+  try {
+    const data = await supabase.from("sitters").select("*");
+    res.json({ data });
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
