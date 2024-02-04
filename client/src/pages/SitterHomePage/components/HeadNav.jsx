a469e17142dde05817c36e40df8143c3d08a60db
Supakit <book0970403815@hotmail.com>
2024-02-04 23:21:08 +0700

    feat: fix headnav style

--- client/src/pages/SitterHomePage/components/HeadNav.jsx
+++ client/src/pages/SitterHomePage/components/HeadNav.jsx
@@ -1,52 +1,36 @@
 import React from "react";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import marieimg from "../../../PublicPicture/marieimg.png";
 
 const TopBar = () => {
-  const topBarStyle = css`
+  const containerHeadNavStyle = css`
     background-color: #ffffff;
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-    width: 1630px;
+    width: 1200px;
     height: 72px;
-    padding: 16px;
+    padding: 0px 60px;
     display: flex;
     align-items: center;
+    gap: 8px;
   `;
 
   const logoStyle = css`
     width: 40px;
     height: 40px;
     border-radius: 999px;
-    background: url(<path-to-image>), lightgray 50% / cover no-repeat;
   `;
 
-  const navigationStyle = css`
-    display: flex;
-    gap: 16px;
-  `;
-
-  const fontStyle = css`
-    text-decoration: none;
-    color: #333333;
+  const nameStyle = css`
+    color: rgba(58, 59, 70, 1);
     font-size: 16px;
     font-weight: 500;
   `;
 
-  const proFilebox = css`
-    display: flex;
-    width: 1200px;
-    height: 72px;
-    padding: 16px 60px;
-    justify-content: space-between;
-    align-items: center;
-    flex-shrink: 0;
-  `;
   return (
-    <div css={topBarStyle}>
-      <img src="/src/PublicPicture/logositter.png" alt="Logo" css={logoStyle} />
-      <div css={proFilebox}>
-        <p css={fontStyle}>Name Surname</p>
-      </div>
+    <div className="container_headnav" css={containerHeadNavStyle}>
+      <img src={marieimg} alt="Logo" css={logoStyle} />
+      <p css={nameStyle}>Name Surname</p>
     </div>
   );
 };
