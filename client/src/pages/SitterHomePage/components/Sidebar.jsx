4c99d947cadbcc4594b35d18fe24228e187da489
Supakit <book0970403815@hotmail.com>
2024-02-04 12:07:57 +0700

    feat: fix sidebar style and refactor

--- client/src/pages/SitterHomePage/components/Sidebar.jsx
+++ client/src/pages/SitterHomePage/components/Sidebar.jsx
@@ -1,103 +1,102 @@
-import React from "react";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import {
+  containerSidebarStyle,
+  sectionLogoStyle,
+  fontSidebarStyle,
+  sidebarButtonStyle,
+  sectionLogoutStyle,
+} from "./SidebarStyle";
 
 const Sidebar = ({ activeTap, setActiveTap }) => {
-  const sidebarStyle = css`
-    background-color: #fafafb;
-    display: flex;
-    width: 240px;
-    height: 1024px;
-    flex-direction: column;
-    border-right-color: #dcdfed;
-  `;
+  const [defaultActiveTap, setDefaultActiveTap] =
+    useState("pet-sitter-profile");
 
-  const containerLogo = css`
-    display: flex;
-    padding: 24px;
-    border-radius: 8px;
-    background-color: #fafafb;
-    margin-bottom: 16px;
-  `;
-
-  const logoutStyle = css`
-    margin-top: auto;
-  `;
-
-  const containerButton = css`
-    padding: 10px;
-    align-items: center;
-    margin-left: 10px;
-    justify-content: center;
-    margin-top: 10px;
-  `;
-
-  const fontStyleSidebar = css`
-    display: inline-block;
-    margin-left: 30px;
-    vertical-align: middle;
-    justify-content: center;
-    color: var(--gray-500, #5b5d6f);
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 150%;
-    letter-spacing: -0.32px;
-  `;
-  const fixcenter = css`
-    justify-content: center;
-  `;
+  useEffect(() => {
+    setActiveTap(defaultActiveTap);
+  }, [defaultActiveTap, setActiveTap]);
 
   return (
-    <div css={sidebarStyle}>
-      <div css={containerLogo}>
+    <div className="container_sidebar" css={containerSidebarStyle}>
+      <div className="section__logo" css={sectionLogoStyle}>
         <div className="logo">
           <img src="/src/PublicPicture/logositter.png" alt="Logo" width="140" />
         </div>
       </div>
 
-      <div
-        css={containerButton}
-        onClick={() => {
-          setActiveTap("pet-sitter-profile");
-        }}
-      >
-        <div css={fixcenter}>
-          <img src="/src/PublicPicture/llogohuman.png" alt="Human" width="20" />
-          <p css={fontStyleSidebar}>Pet Sitter Profile</p>
+      <div className="section__sidebar-button">
+        <div
+          className="sidebar"
+          css={sidebarButtonStyle}
+          onClick={() => {
+            setActiveTap("pet-sitter-profile");
+          }}
+          style={{
+            backgroundColor:
+              activeTap === "pet-sitter-profile"
+                ? "rgba(255, 241, 236, 1)"
+                : "transparent",
+          }}
+        >
+          <img
+            src="/src/PublicPicture/llogohuman.png"
+            alt="Human"
+            width="24"
+            height="24px"
+          />
+          <p css={fontSidebarStyle}>Pet Sitter Profile</p>
         </div>
-      </div>
 
-      <div
-        css={containerButton}
-        onClick={() => {
-          setActiveTap("booking-list");
-        }}
-      >
-        <div css={fixcenter}>
-          <img src="/src/PublicPicture/list.png" alt="booking" width="30" />
-          <p css={fontStyleSidebar}>Booking List</p>
+        <div
+          className="sidebar"
+          css={sidebarButtonStyle}
+          onClick={() => {
+            setActiveTap("booking-list");
+          }}
+          style={{
+            backgroundColor:
+              activeTap === "booking-list"
+                ? "rgba(255, 241, 236, 1)"
+                : "transparent",
+          }}
+        >
+          <img
+            src="/src/PublicPicture/bookingicon.png"
+            alt="booking"
+            width="24"
+            height="24px"
+            css={css`
+              filter: grayscale(100%);
+            `}
+          />
+          <p css={fontSidebarStyle}>Booking List</p>
         </div>
-      </div>
 
-      <div
-        css={containerButton}
-        onClick={() => {
-          setActiveTap("payout-option");
-        }}
-      >
-        <div>
+        <div
+          className="sidebar"
+          css={sidebarButtonStyle}
+          onClick={() => {
+            setActiveTap("payout-option");
+          }}
+          style={{
+            backgroundColor:
+              activeTap === "payout-option"
+                ? "rgba(255, 241, 236, 1)"
+                : "transparent",
+          }}
+        >
           <img
             src="/src/PublicPicture/payouticon.png"
             alt="payout"
-            width="30"
+            width="24"
+            height="24px"
           />
-          <p css={fontStyleSidebar}>Payout Option</p>
+          <p css={fontSidebarStyle}>Payout Option</p>
         </div>
       </div>
 
-      <div css={logoutStyle}>
+      <div className="section__logout" css={sectionLogoutStyle}>
         <div className="logout">
           <img src="/src/PublicPicture/logout.png" alt="logout" width="240" />
         </div>
